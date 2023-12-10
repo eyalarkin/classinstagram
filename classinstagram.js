45592b512e4155f7247fe828d51ee6950310572b
Eyal Arkin <eyal@arkinnet.com>
2023-12-09 22:50:52 -0500

    functionality

--- classinstagram.js
+++ classinstagram.js
@@ -10,3 +10,47 @@ const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const dbName  = process.env.MONGO_DB_NAME;
 const collection = process.env.MONGO_DB_COLLECTION;
+
+const dbAndCollection = {db: dbName, collection: collection};
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `get uri`;
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+const app = express();
+
+app.set('views', path.resolve(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('templates'));
+
+const port = 443;
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/submit', (req, res) => {
+    res.render("submit");
+});
+
+app.post('/submit', (req, res) => {
+    res.render("confirmSubmission");
+});
+
+app.get('/errorSubmission', (req, res) => {
+    res.render("errorSubmission");
+});
+
+app.get('/identity', (req, res) => {
+    res.render("adminConfirm");
+});
+
+app.post('/admin', (req, res) => {
+    res.render("admin");
+});
+
+app.get('/admin', (req, res) => {
+    res.send("uh oh");
+});
