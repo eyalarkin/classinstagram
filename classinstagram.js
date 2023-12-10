0ccb083b2047128ccf9980f4e189809e237568c0
Eyal Arkin <eyal@arkinnet.com>
2023-12-09 19:39:11 -0500

    updates

--- classinstagram.js
+++ classinstagram.js
@@ -1,1 +1,12 @@
-console.log("cool");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const dbName  = process.env.MONGO_DB_NAME;
+const collection = process.env.MONGO_DB_COLLECTION;
